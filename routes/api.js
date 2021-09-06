29544433e0e63f30d0d86fed9ea53789e7e8c51a
Javier <javigh87@gmail.com>
2021-09-06 21:42:12 +0200

    Añadimos la ruta de la api con filtros

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,20 @@
 
 const apiRouter = require('express').Router()
 const Ad = require('../models/ad')
+const { getFilteredQuery } = require('../utils/getQuery')
 
 apiRouter.get('/', async (req, res, next) => {
   // Obtener todos los articulos
   const ads = await Ad.find()
   res.json(ads)
+})
+
+apiRouter.get('/anuncios', async (req, res, next) => {
+  // Articulos filtrados en API
+  const [ query, optionals ] = getFilteredQuery(req.query)
+  const adsFiltered = await Ad.find(query, null, optionals)
+  
+  res.json(adsFiltered)
 })
 
 apiRouter.post('/', (req, res, next) => {
