04c37bc9e200dc9a889af11c350a59608e043d8f
Javier <javigh87@gmail.com>
2021-09-07 09:52:55 +0200

    Añadidas las validaciones del modelo ad

--- models/ad.js
+++ models/ad.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose')
 
 const adSchema = new mongoose.Schema({
-  nombre: String,
-  venta: Boolean,
-  precio: Number,
+  nombre:{
+    type: String,
+    required: [true, 'Nombre del anuncio requerido']
+  } ,
+  venta: {
+    type: Boolean,
+    required: [true, 'Tipo de venta requerido']
+  },
+  precio: {
+    type: Number,
+    required: [true, 'Precio del articulo requerido']
+  },
   photo: String,
-  tag: [String]
+  tags: {
+    type: [String],
+    enum: {
+      values: ['lifestyle', 'work', 'mobile', 'motor'],
+      message: '{VALUE} no es un tag válido'
+    },
+    validate: {
+      validator: (v) => v.length !== 0, // Validamos que no venga vacío el arr
+      message: 'Ha de tener 1 tag mínimo'
+    }
+  }
 })
 
 /*
