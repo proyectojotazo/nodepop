89b0663d028cd5f18899e415ef8cece7c68367c9
Javier <javigh87@gmail.com>
2021-09-11 14:35:32 +0200

    Añadido validador de tags en modelo Ad

---
+++ models/customValidators.js
@@ -0,0 +1,18 @@
+const tagsValidators = [
+  {
+    validator: (v) => v.every(tag => tag !== ''),
+    message: 'No puede contener tags vacíos'
+  },
+  {
+    validator: (v) => v.length !== 0,
+    message: 'Debe contener mínimo 1 tag'
+  },
+  {
+    validator: (v) => v.length < 4,
+    message: 'Debe contener maximo 4 tags'
+  }
+]
+
+module.exports = {
+  tagsValidators
+}
