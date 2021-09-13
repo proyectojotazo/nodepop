9576b774268bfd892be82c9b50f8367344c22613
Javier <javigh87@gmail.com>
2021-09-13 06:26:57 +0200

    Añadido fullads, con varios anuncios. Configurado para funcionar con 'install_db'

--- install_db.js
+++ install_db.js
@@ -1,38 +1,49 @@
 const mongoose = require('mongoose')
 const asyncHandler = require('express-async-handler')
+const argvs = process.argv
 
 const Ad = require('./models/ad')
 
 const { MONGODB_URI } = require('./utils/config')
 
 const { ads } = require('./ads.json')
+const { fullads } = require('./fullads.json')
 
 
 const deleteAllData = asyncHandler(async () => {
-  
+
   await Ad.deleteMany()
   console.log('All data successfully deleted')
-  
+
 })
 
 const loadDefaultData = asyncHandler(async () => {
-  
-  await Ad.insertMany(ads)
+  const adsToAdd = argvs[2] === 'fullads' ? fullads : ads
+  await Ad.insertMany(adsToAdd)
   console.log('Data inserted successfully')
   process.exit()
 
 })
 
-mongoose.connect(MONGODB_URI)
-  .then(asyncHandler(async() => {
-    console.info('Connected to MongoDB')
+const main = asyncHandler(async () => {
+  await mongoose.connect(MONGODB_URI)
+  console.info('Connected to MongoDB')
+  await deleteAllData()
+  await loadDefaultData()
+})
 
-    await deleteAllData()
-    await loadDefaultData()
+main()
 
-  }))
-  .catch(err => {
-    console.error('Error connecting to MongoDB\n', err.message)
-  })
+// mongoose.connect(MONGODB_URI)
+//   .then(asyncHandler(async() => {
+//     console.info('Connected to MongoDB')
+
+//     await deleteAllData()
+//     await loadDefaultData()
+
+//   }))
+//   .catch(err => {
+//     console.error('Error connecting to MongoDB\n', err.message)
+//   })
 
 
